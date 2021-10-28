ac276d33614d5266a7c5ac0515adf11cd036bec4
Suki Shergill <sks78@sfu.ca>
2021-10-28 13:32:14 -0700

    Add files via upload

--- simulation.m
+++ simulation.m
@@ -27,15 +27,16 @@ Fluid.S_gcr = 0.15;     % critical gas saturation
 
 Fluid.sigma = 0.0623;   % interfacial tension of air/water
 
-Fluid.P_cdim = 0.18557;       % dimensionless capillary pressure
-Fluid.P_D = 5.31*10^2;        % displacement pressure
+Fluid.P_cdim = 0.18557;                         % dimless cap pressure
+Fluid.P_D = 5.31*10^2;                          % displacement pressure
+P_w = Fluid.rho_w*9.8*Grid.z + 1.01*10^5;       % water pressure
 
 % domain
 Grid.x = 20;    Grid.dx = 0.1;      Grid.Nx = Grid.x/Grid.dx;
 Grid.z = 5;     Grid.dz = 0.05;     Grid.Nz = Grid.z/Grid.dz;
 
-Grid.t = 0;                  % start time
-Grid.t_end = 1296000;        % end time
+t = 0;                  % start time
+t_end = 1296000;        % end time
 Grid.dt = 0.1;               % time step
 
 % initial values
@@ -45,20 +46,24 @@ T = 10*ones(Grid.Nz,Grid.Nx);
 % sink term that represents the heat comsumed by co-boiling
 Q = zeros(Grid.Nz, Grid.Nx);      
 
-% !!!!!!!!!!!!!!!!!!!
-% INCOMPLETE
-% !!!!!!!!!!!!!!!!!!!
-% Initial water and NAPL saturations are not given
-S_g = zeros(Grid.Nz,Grid.Nx);     % initial gas saturation
+% Initial saturations
+S_g = zeros(Grid.Nz,Grid.Nx);         % initial gas saturation
 S_w = 0.5*ones(Grid.Nz,Grid.Nx);      % initial water saturation
 S_n = 0.5*ones(Grid.Nz,Grid.Nx);      % initial NAPL saturation
 
-% !!!!!!!!!!!!!!!!!!!
-% INCOMPLETE
-% !!!!!!!!!!!!!!!!!!!
+% initial moles of water and NAPL
+n_w = zeros(Grid.Nz,Grid.Nx);     % moles of water
+n_n = zeros(Grid.Nz,Grid.Nx);     % moles of NAPL
+
 % heat flux due to the heaters
-f_ext = 0;        % exterior heaters
-f_int = 0;        % interior heaters
+f = zeros(Grid.Nz, Grid.Nx);
+
+f(:,1) = -20*ones(1,Grid.Nz);
+f(:,end) = -1*f(:,1);
+f(:,50) = 40*ones(1,Grid.Nz);
+f(:,100) = f(:,50);
+f(:,150) = f(:,50);
+
 
 % initial K_e
 K_e = (kappa*(1 - S_g))./(1 + (kappa - 1)*(1 - S_g));      
@@ -69,37 +74,62 @@ lambda = K_e*(lambda_sat - lambda_dry) + lambda_dry;
 % initial heat capacity
 heat_cap = S_w*Fluid.por*Fluid.rho_w*Fluid.C_pw + ...
     S_n*Fluid.por*Fluid.rho_n*Fluid.C_pn + ...
-    (1-Fluid.por)*Fluid.rho_s*Fluid.C_ps;                 
+    (1-Fluid.por)*Fluid.rho_s*Fluid.C_ps;    
 
-%% 
-T = temp(Grid, T, Q, lambda, heat_cap, f_ext, f_int);
-
-%%
-[n_gn, n_gw] = NAPL_mole(Fluid, T);
-
-%%
-Sg = macroIP(S_w, S_g, S_n, Fluid, Grid, T);
 
 %%
 while t <= t_end
     
     % compute temp
-    T = temp(Grid, T, Q, lambda, heat_cap, f_ext, f_int);
+    T = temp(Grid, T, Q, lambda, heat_cap, f);
     
-    % compute moles of NAPL vapor
-    %[n_gn, n_gw] = NAPL_mole(Fluid, T);
+    % compute vapor pressure using Antoine eqn
+    P_wv = 10.^(8.07131*ones(size(T)) - 1730.63*ones(size(T))...
+        ./(233.426*ones(size(T)) + T));
+    P_nv = 10.^(7.11*ones(size(T)) - 1367.05*ones(size(T))...
+        ./(235.809*ones(size(T))+T));
     
-    S_g = macroIP(S_w, S_g, S_n, Fluid, Grid, T);
+    % check if T reaches co-boiling temp
+    if max(P_wv + P_nv) >= (P_w + Fluid.P_D)
+         % compute mole fractions
+         y_n = P_nv ./ (P_nv + P_wv);       % NAPL mole fraction
+         y_w = P_nw ./ (P_nv + P_wv);       % water mole fraction
+         
+         % compute moles of water vapor and NAPL steam produced
+         n_gn = y_n .* n_n;     % NAPL vapor
+         n_gw = y_w .* n_w;     % water vapor
+         
+         % update moles of NAPL and water (liquid phase)
+         n_n = n_n - n_gn;
+         n_w = n_w - n_gw;
+         
+         % compute Q
+         Q = (Fluid.L_w * n_gw + Fluid.L_n * n_gn) /...
+             (Grid.dx * Grid.dz * Grid.dt);
+         
+         % compute volume of gas using the ideal gas law
+         V_n = (n_gn.*T) ./ P_nv;         % NAPL vapor
+         V_w = (n_gw.*T) ./ P_wv;         % water vapor
+         
+         
+         % !!!!!!!!!!!!!!!!!!!
+         % INCOMPLETE
+         % !!!!!!!!!!!!!!!!!!!
+         % Need to express volume of gas in terms of saturation, which will
+         % update S_g. Also need to update S_n and S_w using the moles of
+         % gas produced and/or the equilibium equations S_n + S_w + S_g = 1        
+         
+         % update heat capacity
+         heat_cap = S_w*Fluid.por*Fluid.rho_w*Fluid.C_pw + ...
+             S_n*Fluid.por*Fluid.rho_n*Fluid.C_pn + ...
+             (1-Fluid.por)*Fluid.rho_s*Fluid.C_ps;
 
-    % !!!!!!!!!!!!!!!!!!!
-    % INCOMPLETE
-    % !!!!!!!!!!!!!!!!!!!
-    % NEED TO UPDATE HEAT CAPACITY, S_n and S_w
-    % use equilibrium equations S_n + S_w + S_g = 1
-    
-    % update thermal conductivity
-    K_e = (kappa*(1 - S_g))/(1 + (kappa - 1)*(1 - S_g));    
-    lambda = K_e*(lambda_sat - lambda_dry) + lambda_dry;
+         % update thermal conductivity
+         K_e = (kappa*(1 - S_g))/(1 + (kappa - 1)*(1 - S_g));    
+         lambda = K_e*(lambda_sat - lambda_dry) + lambda_dry;
+         
+         S_g = macroIP(S_w, S_g, S_n, Fluid, Grid, T);
+    end
 
-    t = t + dt;
+    t = t + Grid.dt;
 end
